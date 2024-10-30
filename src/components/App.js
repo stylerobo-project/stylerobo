012f088b45f30a76375d8119062c8acfea4df874
milka48 <dbwls020906@gmail.com>
2024-10-31 00:22:19 +0900

    nav, router 설정

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "../pages/HomePage";
+import Favorite from "../pages/Favorite.js";
 import Nav from "../components/Nav";
 import Footer from "../components/Footer";
 import styles from "./App.module.css";
 import "./App.font.css";
 
-function App({ children }) {
+function App() {
   return (
-    <>
+    <Router>
       <Nav className={styles.nav} />
-      <div className={styles.body}>{children}</div>
+      <div className={styles.body}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/favorite" element={<Favorite />} />
+        </Routes>
+      </div>
       <Footer className={styles.footer} />
-    </>
+    </Router>
   );
 }
 
