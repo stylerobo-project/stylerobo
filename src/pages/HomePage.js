25b033e0a44688d512596837db46a10b404b7d48
milka48 <132130727+milka48@users.noreply.github.com>
2024-12-10 16:24:07 +0900

    pages 수정

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,3 +1,114 @@
+// import Container from "../components/Container";
+// import styles from "./HomePage.module.css";
+// import SeasonImg from "../assets/season.png";
+// import RobotImg from "../assets/robot.png";
+// import LineImg1 from "../assets/line1.png";
+// import LineImg2 from "../assets/line2.png";
+// import LineImg3 from "../assets/line3.png";
+// import LineImg4 from "../assets/line4.png";
+// import Circle from "../assets/circle.png";
+// import Circle1 from "../assets/circle1.png";
+// import Circle2 from "../assets/circle2.png";
+
+// function HomePage() {
+//   return (
+//     <>
+//       <div className={styles.bg} />
+//       <Container className={styles.containerTitle}>
+//         <div className={styles.circle1}>
+//           <img src={Circle1} alt="circle1" />
+//         </div>
+//         <div className={styles.circle2}>
+//           <img src={Circle2} alt="circle2" />
+//         </div>
+//         <div className={styles.circle3}>
+//           <img src={Circle} alt="circle" />
+//         </div>
+//         <div className={styles.circle4}>
+//           <img src={Circle} alt="circle" />
+//         </div>
+//         <div className={styles.circle5}>
+//           <img src={Circle} alt="circle" />
+//         </div>
+//         <div className={styles.texts}>
+//           <h1 className={styles.heading}>
+//             <strong>
+//               STYLE
+//               <br />
+//               &emsp; &emsp;ROBO
+//             </strong>
+//           </h1>
+//           <p className={styles.description}>나의 패션을 찾아주는 ai 스타일러</p>
+//         </div>
+//         <div className={styles.line1}>
+//           <img src={LineImg1} alt="line1" />
+//         </div>
+//         <div className={styles.line2}>
+//           <img src={LineImg2} alt="line2" />
+//         </div>
+//         <div className={styles.line3}>
+//           <img src={LineImg3} alt="line3" />
+//         </div>
+//         <div className={styles.line4}>
+//           <img src={LineImg4} alt="line4" />
+//         </div>
+//       </Container>
+
+//       <Container className={styles.container}>
+//         <div className={styles.texts}>
+//           <h1 className={styles.title}>
+//             <strong>Personal Color</strong>
+//           </h1>
+//           <p className={styles.subTitle}>
+//             나의 컬러를 찾고 스타일을 추천받아보세요!
+//           </p>
+//           <div className={styles.titleDescription}>
+//             <p>
+//               AI 이미지 분석을 통해 채도와 명도를 분석하여
+//               <br /> 사용자의 얼굴 톤을 파악하고,
+//               <br /> 개인의 퍼스널 컬러에 맞는 스타일을 추천해드립니다.
+//               <br />
+//               <br /> 이 서비스는 개인 맞춤형 패션을 제공하여
+//               <br /> 각자의 독특한 아름다움을 더욱 돋보이게 해주는
+//               <br /> 스타일링 솔루션입니다.
+//             </p>
+//           </div>
+//         </div>
+//         <div className={styles.figure1}>
+//           <img src={SeasonImg} alt="봄여름가을겨울" />
+//         </div>
+//       </Container>
+
+//       <Container className={styles.container}>
+//         <div className={styles.figure2}>
+//           <img src={RobotImg} alt="로봇이미지" />
+//         </div>
+//         <div className={styles.texts}>
+//           <h1 className={styles.title}>
+//             <strong>Favorite fashion</strong>
+//           </h1>
+//           <p className={styles.subTitle}>
+//             나의 취향을 보여주고 스타일을 추천받아보세요!
+//           </p>
+//           <div className={styles.titleDescription}>
+//             <p>
+//               체형과 얼굴형, 스타일을 선택한 후 사용자가 작성한 글에 기반하여{" "}
+//               AI가 취향을 분석하고, 그에 맞춘 맞춤형 코디를 추천합니다.
+//               <br />
+//               <br />
+//               단순히 취향을 찾는 것을 넘어, 사용자의 개인적인 스타일을 <br />
+//               세밀하게 파악하여 체형과 얼굴형에 어울리는 옷을 추천함으로써,
+//               패션에서도 개성을 더욱 표현할 수 있도록 돕습니다.
+//             </p>
+//           </div>
+//         </div>
+//       </Container>
+//     </>
+//   );
+// }
+
+// export default HomePage;
+
 import Container from "../components/Container";
 import styles from "./HomePage.module.css";
 import SeasonImg from "../assets/season.png";
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,3 +1,102 @@
+// import React, { useState } from "react";
+// import { useNavigate } from "react-router-dom";
+// import Container from "../components/Container";
+// import styles from "./Login.module.css";
+// import Button from "../components/Button";
+// import robot from "../assets/robot3.png";
+
+// function Login({ onLoginSuccess }) {
+//   const navigate = useNavigate();
+//   const [username, setUserName] = useState("");
+//   const [password, setPassword] = useState("");
+
+//   const handleLogin = async (e) => {
+//     e.preventDefault();
+
+//     try {
+//       const response = await fetch("http://localhost:8080/api/user/login", {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({ userName: username, password }),
+//         credentials: "include", // 쿠키 포함
+//       });
+
+//       const data = await response.json();
+//       console.log("로그인 응답:", response.headers.get("Set-Cookie")); // 쿠키 확인
+
+//       if (data.statusCode === 200 && data.data.status === 1) {
+//         alert("로그인 성공! 환영합니다!");
+//         await onLoginSuccess(); // 상태 업데이트
+//         navigate("/"); // 홈으로 이동
+//       } else {
+//         alert("로그인 실패: " + data.responseMessage);
+//       }
+//     } catch (error) {
+//       console.error("Error:", error);
+//       alert("로그인 중 오류가 발생했습니다.");
+//     }
+//   };
+
+//   const navSignup = () => {
+//     navigate("/auth/signup");
+//   };
+
+//   return (
+//     <>
+//       <div className={styles.bg} />
+//       <Container className={styles.container}>
+//         <div className={styles.texts}>
+//           <Container className={styles.welcomecontainer}>
+//             <strong>
+//               반갑습니다! 당신의 패션 스타일을 찾아주는 스타일 로보입니다.
+//             </strong>
+//             <div className={styles.figure}>
+//               <img src={robot} alt="로봇 이미지" />
+//             </div>
+//             <div className={styles.heading}>STYLEROBO</div>
+//           </Container>
+
+//           <Container className={styles.logincontainer}>
+//             <div className={styles.heading2}>스타일로보 로그인</div>
+//             <form onSubmit={handleLogin}>
+//               <input
+//                 type="text"
+//                 id="username"
+//                 placeholder="아이디"
+//                 className={styles.inputcontainer}
+//                 value={username}
+//                 onChange={(e) => setUserName(e.target.value)}
+//               />
+//               <input
+//                 type="password"
+//                 id="password"
+//                 placeholder="비밀번호"
+//                 className={styles.inputcontainer}
+//                 value={password}
+//                 onChange={(e) => setPassword(e.target.value)}
+//               />
+//               <Button className={styles.button1} type="submit">
+//                 로그인하기
+//               </Button>
+//               <Button className={styles.button2} onClick={navSignup}>
+//                 회원가입하기
+//               </Button>
+//               <div className={styles.text2}>
+//                 아이디 혹은 비밀번호를 잊으셨나요?
+//                 <div className={styles.text3}>아이디/비밀번호 찾기</div>
+//               </div>
+//             </form>
+//           </Container>
+//         </div>
+//       </Container>
+//     </>
+//   );
+// }
+
+// export default Login;
+
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Container from "../components/Container";
